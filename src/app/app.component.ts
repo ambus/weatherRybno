0d7d0a2c33b645af676061f86bd6e134ed11c150
Szymon Standarski <s.standarski@gmail.com>
2020-10-10 16:20:03 +0200

    fix(add test notification):

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private swPush: SwPush) {
+    console.log('try to show notification');
+    Notification.requestPermission(function (result) {
+      console.log('User choice', result);
+      if (result !== 'granted') {
+        console.log('No notification permission granted!');
+      } else {
+        navigator.serviceWorker.getRegistration().then((reg) => {
+          reg
+            .showNotification('Test', {
+              body: 'jakas tresc',
+              icon: 'assets/icon/favicon.png',
+              // actions: <any>notification.actions,
+              // data: notification.data,
+              vibrate: [100, 50, 10, 20, 20]
+            })
+            .then(
+              (res) => {
+                console.log('showed notification', res);
+              },
+              (err) => {
+                console.error(err);
+              }
+            );
+        });
+      }
+    });
+  }
   title = 'weatherRybno';
 }
